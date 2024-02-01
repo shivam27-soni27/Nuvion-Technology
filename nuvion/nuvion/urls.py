2f7d4731301083dd9dee398c34a125bf12af8069
shivam27-soni27 <94952546+shivam27-soni27@users.noreply.github.com>
2024-02-01 19:00:44 +0530

    update

---
+++ nuvion/nuvion/urls.py
@@ -0,0 +1,10 @@
+# yourproject/urls.py
+from django.contrib import admin
+from django.urls import include, path
+from nuvion import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('home/',views.home),
+    path('workwithus/', include('workwithus.urls')),
+]
